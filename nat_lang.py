00000ad873cc569bb83d24a9902cb746dfb5b1c5
Ian <isf6741@g.rit.edu>
2020-04-29 14:19:33 -0400

    Update NLP to Remove Stopwords
    Add stopword removal to NLP file to increase accuracy of keyword tagging.

--- nat_lang.py
+++ nat_lang.py
@@ -8,20 +8,24 @@ def nlp_process(text):
     tokenizer = RegexpTokenizer(r'\w+')
     tokenized_string = tokenizer.tokenize(text)
     tokenized_string = list(map(lambda x: x.lower(), tokenized_string))
+    stop_words = set(stopwords.words('english')) 
+
+    # Stopword Removal
+    stop_removed = []
+    for word in tokenized_string:
+        if word not in stop_words:
+            stop_removed.append(word)
 
     # Count the frequency for words
-    fdist1 = FreqDist(tokenized_string)
+    fdist1 = FreqDist(stop_removed)
     common_word = fdist1.most_common(1)
-    print(common_word[0])
-    #print(f'The word {common_word[0]} appeared {common_word[1]} times in the text')
+    top_word, top_freq = common_word[0]
+    print("the most common word '{top_word}' occours {top_freq} times in the sampled text.".format(top_word = top_word, top_freq = top_freq))
     print(f'The most common 10 words are: {fdist1.most_common(10)}')
 
     # Get the association between two words. See: http://www.nltk.org/howto/collocations.html
     bigram_measures = BigramAssocMeasures()
-    finder = BigramCollocationFinder.from_words(tokenized_string)
+    finder = BigramCollocationFinder.from_words(stop_removed)
     finder.apply_freq_filter(3)
     finder.apply_word_filter(lambda x: x in stopwords.words('english'))
     print(f'The most correlated words are: {finder.nbest(bigram_measures.pmi, 10)}')
-
-    #print(f'\n\n{"#"*20}')
-    #print(f'Based on abstract:\n{abstract}')
